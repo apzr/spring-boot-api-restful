444d571ceefbce5d6d3d3939e4b250e6c3c49b84
April Z <4961561+apzr@users.noreply.github.com>
2021-12-08 10:55:19 +0800

    format error log
    Signed-off-by: April Z <4961561+apzr@users.noreply.github.com>

--- src/main/java/com/uxlt/project/configurer/WebMvcConfig.java
+++ src/main/java/com/uxlt/project/configurer/WebMvcConfig.java
@@ -7,10 +7,9 @@ import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import com.uxlt.project.core.Result;
 import com.uxlt.project.core.ResultCode;
 import com.uxlt.project.core.ServiceException;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
@@ -21,7 +20,7 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.ServletException;
@@ -38,9 +37,9 @@ import java.util.List;
  * Spring MVC 配置
  */
 @Configuration
-public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
+@Slf4j
+public class WebMvcConfig implements WebMvcConfigurer {
 
-    private final Logger logger = LoggerFactory.getLogger(WebMvcConfigurer.class);
     @Value("${spring.profiles.active}")
     private String env;//当前激活的配置文件
 
@@ -64,35 +63,36 @@ public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
     //统一异常处理
     @Override
     public void configureHandlerExceptionResolvers(List<HandlerExceptionResolver> exceptionResolvers) {
-        exceptionResolvers.add(new HandlerExceptionResolver() {
-            public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception e) {
-                Result result = new Result();
-                if (e instanceof ServiceException) {//业务失败的异常，如“账号或密码错误”
-                    result.setCode(ResultCode.FAIL).setMessage(e.getMessage());
-                    logger.info(e.getMessage());
-                } else if (e instanceof NoHandlerFoundException) {
-                    result.setCode(ResultCode.NOT_FOUND).setMessage("接口 [" + request.getRequestURI() + "] 不存在");
-                } else if (e instanceof ServletException) {
-                    result.setCode(ResultCode.FAIL).setMessage(e.getMessage());
-                } else {
-                    result.setCode(ResultCode.INTERNAL_SERVER_ERROR).setMessage("接口 [" + request.getRequestURI() + "] 内部错误，请联系管理员");
-                    String message;
-                    if (handler instanceof HandlerMethod) {
-                        HandlerMethod handlerMethod = (HandlerMethod) handler;
-                        message = String.format("接口 [%s] 出现异常，方法：%s.%s，异常摘要：%s",
-                                request.getRequestURI(),
-                                handlerMethod.getBean().getClass().getName(),
-                                handlerMethod.getMethod().getName(),
-                                e.getMessage());
-                    } else {
-                        message = e.getMessage();
-                    }
-                    logger.error(message, e);
-                }
-                responseResult(response, result);
-                return new ModelAndView();
-            }
+        exceptionResolvers.add((request, response, handler, e) -> {
+            Result result = new Result();
 
+            if (e instanceof ServiceException) {//业务失败的异常，如“账号或密码错误”
+                result.setCode(ResultCode.FAIL).setMessage(e.getMessage());
+                log.info(e.getMessage());
+            } else if (e instanceof NoHandlerFoundException) {
+                result.setCode(ResultCode.NOT_FOUND).setMessage("接口 [" + request.getRequestURI() + "] 不存在");
+            } else if (e instanceof ServletException) {
+                result.setCode(ResultCode.FAIL).setMessage(e.getMessage());
+            } else {
+                result.setCode(ResultCode.INTERNAL_SERVER_ERROR).setMessage("接口 [" + request.getRequestURI() + "] 内部错误，请查看日志");
+                String message;
+                if (handler instanceof HandlerMethod) {
+                    HandlerMethod handlerMethod = (HandlerMethod) handler;
+                    message = String.format("接口 [%s] 出现异常，方法：%s.%s，异常摘要：%s",
+                            request.getRequestURI(),
+                            handlerMethod.getBean().getClass().getName(),
+                            handlerMethod.getMethod().getName(),
+                            e.getMessage());
+                } else {
+                    message = e.getMessage();
+                }
+                long errId = Thread.currentThread().getId();
+                log.error("*****************Error"+errId+"-Start*****************");
+                log.error(message, e);
+                log.error("******************Error"+errId+"-End******************");
+            }
+            responseResult(response, result);
+            return new ModelAndView();
         });
     }
 
@@ -115,7 +115,7 @@ public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
                     if (pass) {
                         return true;
                     } else {
-                        logger.warn("签名认证失败，请求接口：{}，请求IP：{}，请求参数：{}",
+                        log.warn("签名认证失败，请求接口：{}，请求IP：{}，请求参数：{}",
                                 request.getRequestURI(), getIpAddress(request), JSON.toJSONString(request.getParameterMap()));
 
                         Result result = new Result();
@@ -135,7 +135,7 @@ public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
         try {
             response.getWriter().write(JSON.toJSONString(result));
         } catch (IOException ex) {
-            logger.error(ex.getMessage());
+            log.error(ex.getMessage());
         }
     }
 
